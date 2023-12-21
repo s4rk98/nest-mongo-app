9a91ecd51352898e62242c946a227636dfd216f3
SarathAR <arsarath177@gmail.com>
2023-12-21 19:05:06 +0000

    added docker configuration with replica set

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ClientsModule } from './clients/clients.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/chatster_db'),
+    MongooseModule.forRoot('mongodb://mongodb_cont_1:27017,mongodb_cont_2:27017,mongodb_cont_3:27017/?replicaSet=my_mongodb_replset'),
     ClientsModule,
   ],
 })
